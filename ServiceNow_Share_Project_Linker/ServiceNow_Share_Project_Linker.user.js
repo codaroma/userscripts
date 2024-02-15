0d5ad11009aa0327542b85f6f4b32f151f6ffe33
codaroma <github@codaroma.com>
2024-02-15 23:59:42 +1100

    restructured code and added github references into header

--- ServiceNow_Share_Project_Linker/ServiceNow_Share_Project_Linker.user.js
+++ ServiceNow_Share_Project_Linker/ServiceNow_Share_Project_Linker.user.js
@@ -1,48 +1,42 @@
 // ==UserScript==
 // @name         ServiceNow Share Project Linker
-// @namespace    https://greasyfork.org/en/users/1260574
-// @version      2024-02-13
+// @namespace    https://github.com/codaroma/userscripts
+// @copyright    2024+, codaroma (https://github.com/codaroma)
+// @version      0.0.1
 // @description  Convert ServiceNow Share project links from AngularJS script to regular href links
-// @author       codaroma
-// @match        https://developer.servicenow.com/connect.do
 // @icon         https://developer.servicenow.com/favicon.ico
 // @grant        none
+// @author       codaroma
+// @homepage     https://github.com/codaroma/userscripts/tree/main/ServiceNow_Share_Project_Linker
+// @match        https://developer.servicenow.com/connect.do
+// @updateURL    https://github.com/codaroma/userscripts/raw/main/ServiceNow_Share_Project_Linker/ServiceNow_Share_Project_Linker.user.js
+// @downloadURL  https://github.com/codaroma/userscripts/raw/main/ServiceNow_Share_Project_Linker/ServiceNow_Share_Project_Linker.user.js
+// @supportURL   https://github.com/codaroma/userscripts/issues
 // ==/UserScript==
 
 (function () {
     "use strict";
-    new MutationObserver(observe).observe(document.querySelector("body"), {
+    new MutationObserver(() => {
+        const hashPath = location.hash.split("?", 1)[0];
+        const pathConfig = {
+            "#!/share": {
+                selector:
+                    "div.dp-sh-bottom-box-row ul.card-view-list a.dp-sh-bottom-item-header[data-ng-click='takeToLink(listItem.link,{}, $event)'][href='javascript: void(0)']",
+                getUrl: (scope) => scope.listItem.link,
+            },
+            "#!/share/contents": {
+                selector:
+                    "table.dp-scc-table tbody a.app-title[data-ng-click='shareContentsVM.takeToLink(row.url,{}, $event)'][href='javascript: void(0)']",
+                getUrl: (scope) => scope.row.url,
+            },
+        }[hashPath];
+        if (pathConfig) {
+            $(pathConfig.selector)
+                .off("click")
+                .attr("href", () => pageConfig.getUrl($(this).scope()));
+        }
+    }).observe(document.querySelector("body"), {
         subtree: true,
         childList: true,
     });
-
-    function observe() {
-        fixLinks({
-            "#!/share": {
-                sel: "a.dp-sh-bottom-item-header[data-ng-click='takeToLink(listItem.link,{}, $event)'][href='javascript: void(0)']",
-                ref: "listItem.link",
-            },
-            "#!/share/contents": {
-                sel: "a.app-title[data-ng-click='shareContentsVM.takeToLink(row.url,{}, $event)'][href='javascript: void(0)']",
-                ref: "row.url",
-            },
-        });
-    }
-
-    function fixLinks(config) {
-        const hashPage = location.hash.split("?", 1)[0];
-        const pageConfig = config[hashPage];
-        if (pageConfig) {
-            document.querySelectorAll(pageConfig.sel).forEach(function (elem) {
-                const angElem = angular.element(elem);
-                angElem.off("click");
-                angElem.attr(
-                    "href",
-                    pageConfig.ref
-                        .split(".")
-                        .reduce((p, c) => (p && p[c]) || null, angElem.scope())
-                );
-            });
-        }
-    }
 })();
